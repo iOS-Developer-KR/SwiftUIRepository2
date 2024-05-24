d8830abc1135cfdf4de5a103c38111239cf0c9d9
happyytw <happyytw@gmail.com>
2024-05-24 18:53:45 +0900

    updated GlobalActorExample

--- GlobalActorExample/GlobalActorExample/ContentView.swift
+++ GlobalActorExample/GlobalActorExample/ContentView.swift
@@ -7,24 +7,31 @@
 
 import SwiftUI
 
+@globalActor struct MyGlobalActor {
+    
+    static var shared = MyNewDataManager()
+}
+
 actor MyNewDataManager {
     
     func getDataFromDatabase() -> [String] {
-        
-        return ["One", "Two", "Three"]
+        print(Thread.current)
+        return ["One", "Two", "Three", "Four"]
     }
 }
 
 class GlobalActorViewModel: ObservableObject {
     
-    @Published var dataArray: [String] = []
-    let manager = MyNewDataManager()
+    @MainActor @Published var dataArray: [String] = []
+    let manager = MyGlobalActor.shared
     
+    @MyGlobalActor
     func getData() async {
-        
-        // 엄청 무거운 메서드들
-        let data = await manager.getDataFromDatabase()
-        self.dataArray = data
+        // 엄청 무거운 작업을 Main Thread 에서 작업한다면 문제가 발생
+        let data = await manager.getDataFromDatabase() // actor이므로 자동 async 코드로 인식
+        await MainActor.run(body: {
+             self.dataArray = data
+        })
     }
 }
 
@@ -41,6 +48,9 @@ struct ContentView: View {
                         .font(.headline)
                 }
             }
+        }
+        .onAppear {
+            print(Thread.current)
         }
         .task {
             await viewModel.getData()
